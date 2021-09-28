86f765f12ae6b55999936de9894373868b43c45e
gldnpz17 <firdausbismasuryakusuma@gmail.com>
2021-09-28 23:15:45 +0700

    Auto reattempt connecting to discord.

--- services/discord-logger.js
+++ services/discord-logger.js
@@ -1,27 +1,49 @@
 import { Client, Intents } from "discord.js"
 import loggingLevel from '../common/logging-level.js'
 
+const delay = (duration) => {
+    return Promise((resolve) => {
+        setTimeout(resolve, duration)
+    })
+}
 class DiscordLogger {
     constructor({ discordToken, discordServerId, discordChannelId }) {
         let client = new Client({
             intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
         })
+
         this.channel = null
-        client.login(discordToken).then(() => {
-            client.once('ready', () => {
-                let guild = client.guilds.cache.get(discordServerId)
-                let channel = guild?.channels.cache.get(discordChannelId)
-                
-                if (channel) {
-                    const startMessage = 'Discord logger started!'
-                    console.log(startMessage)
-                    channel.send(startMessage)
-                    this.channel = channel
-                } else {
-                    console.log('Discord logger failed to initialize!')
-                }
+
+        this.attemptLogin(client, discordToken, discordServerId, discordChannelId)
+    }
+
+    async attemptLogin(client, token, serverId, channelId) {
+        console.log('Attempting to connect to discord.')
+        
+        let counter = 0
+        while(!this.channel || counter < 10) {
+            client.login(token).then(() => {
+                client.once('ready', () => {
+                    let guild = client.guilds.cache.get(serverId)
+                    let channel = guild?.channels.cache.get(channelId)
+                    
+                    if (channel) {
+                        const startMessage = 'Discord logger started!'
+                        console.log(startMessage)
+                        channel.send(startMessage)
+                        this.channel = channel
+                    } else {
+                        console.log('Discord logger failed to initialize!')
+                    }
+                })
             })
-        })
+
+            await delay(10000)
+        }
+
+        if (counter >= 10) {
+            console.log('Too many discord connect attempts.')
+        }
     }
     
     log(level, message) {
